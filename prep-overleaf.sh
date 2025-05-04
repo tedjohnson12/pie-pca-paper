19cac84263888dbdc9b5f636fca52d7cdd09e953
Ted Johnson <ted.johnson@unlv.edu>
2025-05-04 12:01:22 -0700

    Allow all syw stuff on overleaf

---
+++ prep-overleaf.sh
@@ -0,0 +1,6 @@
+source_dir=./.showyourwork/compile
+dest_dir=./src/tex
+
+cp $source_dir/showyourwork.tex $dest_dir
+cp $source_dir/showyourwork-metadata.tex $dest_dir
+cp $source_dir/showyourwork-logo.pdf $dest_dir
